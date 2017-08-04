3626dbec44b388185ca70c7a58fe8bc996296c1a
spaar <spaar.modding@gmail.com>
2017-08-04 13:58:13 +0200

    Embed resources into the .dll.

--- Key Manager/Textures.cs
+++ Key Manager/Textures.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using UnityEngine;
 
 namespace spaar.Mods.KeyManager
@@ -15,7 +16,7 @@ namespace spaar.Mods.KeyManager
       Delete = LoadTexture("delete");
     }
 
-    private static Texture2D LoadTexture(string name)
+    /*private static Texture2D LoadTexture(string name)
     {
       var path = Application.dataPath + "/Mods/Resources/KeyManager/" + name + ".png";
 
@@ -32,6 +33,42 @@ namespace spaar.Mods.KeyManager
         Debug.LogException(e);
       }
       return null;
+    }*/
+
+    private static Texture2D LoadTexture(string name)
+    {
+      try
+      {
+        var assembly = Assembly.GetExecutingAssembly();
+        using (var stream = assembly.GetManifestResourceStream($"spaar.Mods.KeyManager.Resources.{name}.png"))
+        {
+          var bytes = ReadAllBytes(stream);
+          var texture = new Texture2D(0, 0);
+          texture.LoadImage(bytes);
+          return texture;
+        }
+      }
+      catch (Exception e)
+      {
+        Debug.LogError($"Error loading texture: {name}");
+        Debug.LogException(e);
+        return null;
+      }
+    }
+
+    private static byte[] ReadAllBytes(Stream stream)
+    {
+      using (var memStream = new MemoryStream())
+      {
+        var buffer = new byte[512];
+        var bytesRead = 0;
+        while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+        {
+          memStream.Write(buffer, 0, bytesRead);
+        }
+
+        return memStream.ToArray();
+      }
     }
   }
 }
